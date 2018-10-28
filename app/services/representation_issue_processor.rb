1e2f25281a2a5932b2572df1430473e9a0929522
jtrtj <jt.r@live.com>
2018-10-28 14:08:30 -0600

    Refactored RepresentationIssueProcessor to not have regex params as instance variables

--- app/services/representation_issue_processor.rb
+++ app/services/representation_issue_processor.rb
@@ -2,14 +2,14 @@ class RepresentationIssueProcessor
   def initialize(rep_params)
     @lobbying_represenntation_id = rep_params[:representation_id]
     @issue                       = rep_params[:issue_string]
-    @stop_words                  = /\b(?:#{ %w[to and or the a of by for on].join('|') })\b/i
-    @allowed_characters          = Regexp.union(/[a-z]/, " ")
   end
 
-  def parse_issue
-    downcased_issue = @issue.downcase
-    issue_with_only_words = downcased_issue.chars.select {|c| c =~ @allowed_characters}.join("")
-    no_stop_words = issue_with_only_words.gsub(@stop_words, '')
-    split_unique_words = no_stop_words.split.uniq
+  def extract_words_from_issue
+    stop_words            = /\b(?:#{ %w[to and or the a of by for on].join('|') })\b/i
+    allowed_characters    = Regexp.union(/[a-z]/, " ")
+    downcased_issue       = @issue.downcase
+    issue_with_only_words = downcased_issue.chars.select {|c| c =~ allowed_characters}.join("")
+    no_stop_words         = issue_with_only_words.gsub(stop_words, '')
+    split_unique_words    = no_stop_words.split.uniq
   end
 end
--- spec/services/representation_issue_processor_spec.rb
+++ spec/services/representation_issue_processor_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe RepresentationIssueProcessor do
   context 'instance methods' do
-    it '.parse_issue - returns the issue string split into words without stop words, punctuation, numbers or repeated words' do
+    it '.extract_words_from_issue - returns the issue string split into words without stop words, punctuation, numbers or repeated words' do
      data = { "representation_id": 1, "issue_string": "the Test issue issue 11345 &#@$% [](" }
      expected = ['test', 'issue']
      processor = RepresentationIssueProcessor.new(data)
 
-     expect(processor.parse_issue).to eq(expected)
+     expect(processor.extract_words_from_issue).to eq(expected)
     end
   end
 end

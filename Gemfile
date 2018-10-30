15de9e8715054c20aed9a336aa0ff0090aed1d80
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-10-30 10:55:57 -0600

    Add sad path for entirely empty issue, add more elaborate word filter

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry'
   gem 'rspec-rails'
   gem 'shoulda-matchers', '~> 3.1'
   gem 'factory_bot_rails'
--- spec/requests/api/v1/post_issue_string_endpoint_spec.rb
+++ spec/requests/api/v1/post_issue_string_endpoint_spec.rb
@@ -12,5 +12,15 @@ describe '/api/v1' do
       expect(status).to be(201)
       expect(result["message"]).to eq("Lobbying Representation: #{payload[:representation_id]} issue logged in common words system.")
     end
+    it "will not add blank issues into the database" do
+      payload = { "representation_id": 1, "issue_string": " boi boi boi boi boi"}
+
+      post '/api/v1/common_words', params: payload
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(400)
+      expect(result[:message]).to eq('Invalid issue parameters')
+    end
   end
 end

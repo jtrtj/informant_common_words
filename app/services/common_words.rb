4189990eb2eda96ec807daab25f9286427dd0aa1
jtrtj <jt.r@live.com>
2018-10-31 15:56:03 -0600

    add additional parameters to common_words query

--- app/services/common_words.rb
+++ app/services/common_words.rb
@@ -1,6 +1,7 @@
 class CommonWords
   def self.data
-    Word.all.map do | word |
+    words = Word.order(representation_issue_count: :desc).where("representation_issue_count > ?", 5).limit(500)
+    words.map do | word |
       { text: word.text, value: word.representation_issue_count }
     end
   end
